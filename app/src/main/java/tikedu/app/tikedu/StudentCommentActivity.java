d4b2b28bb8bf363b93a63bd65b3aa012a0ecff93
Marc Guillaume <marcguillaume17@gmail.com>
2022-03-22 22:07:22 -0400

    add new screens from figma wireframe

---
+++ app/src/main/java/tikedu/app/tikedu/StudentCommentActivity.java
@@ -0,0 +1,16 @@
+package tikedu.app.tikedu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentCommentActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_comment);
+    }
+}
