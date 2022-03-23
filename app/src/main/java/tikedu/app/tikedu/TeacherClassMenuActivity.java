c17821309ed37f2eb9859d267addb57f75eff854
masongreen30 <73558998+masongreen30@users.noreply.github.com>
2022-03-22 22:08:52 -0400

    Added UI for approval/denial of videos and the teacher class menu

---
+++ app/src/main/java/tikedu/app/tikedu/TeacherClassMenuActivity.java
@@ -0,0 +1,21 @@
+package tikedu.app.tikedu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Objects;
+
+public class TeacherClassMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ** ALWAYS REMOVE TITLE BAR FROM ACTIVITY FIRST **
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        // ** ALWAYS REMOVE TITLE BAR FROM ACTIVITY FIRST **
+        setContentView(R.layout.activity_teacher_class_menu);
+    }
+}
